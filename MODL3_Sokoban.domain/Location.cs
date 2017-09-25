56aa73af3658c838b40e98d58e5034250cede139
Arcaev <Jorrit-93@hotmail.com>
2017-09-25 15:25:33 +0200

    movetoloc

--- MODL3_Sokoban.domain/Location.cs
+++ MODL3_Sokoban.domain/Location.cs
@@ -8,22 +8,55 @@ namespace MODL3_Sokoban.domain
 {
 	class Location
 	{
-		public Location leftLoc;
-		public Location rightLoc;
-		public Location upLoc;
-		public Location downLoc;
+		public Symbol role { get; set; }
 		public int xPosition { get; set; }
 		public int yPosition { get; set; }
 
-		public Location(int x, int y)
+		public Location(int x, int y, Symbol role)
 		{
+			this.role = role;
 			xPosition = x;
 			yPosition = y;
 		}
 
-		public void createSurrounding(Maze maze)
+		public Location getLeftLoc(Direction d, Maze m)
 		{
+			int x = xPosition;
+			int y = yPosition;
+			
+			switch (d) {
+				case Direction.left:
+					x = x - 1;
+					break;
+				case Direction.right:
+					x = x + 1;
+					break;
+				case Direction.up:
+					y = y - 1;
+					break;
+				case Direction.down:
+					y = y + 1;
+					break;
+			}
 
+			foreach(Location l in m.locList) {
+				if(l.xPosition == x && l.yPosition == y)
+				{
+					if(Maze.character)
+					switch (l.role)
+					{
+						case Symbol.dot:
+							return l;
+						case Symbol.hashtag:
+							return null;
+						case Symbol.o:
+							return l;
+						case Symbol.zero:
+							return l;
+					}
+				}
+			}
+			return null;
 		}
 	}
 }
