4d0a79f5f556d8171a0e168aafaad10f10afbc33
Unknown <mbeks1@avans.nl>
2017-10-04 16:39:45 +0200

    Movable

--- MODL3_Sokoban.domain/Movable.cs
+++ MODL3_Sokoban.domain/Movable.cs
@@ -8,5 +8,18 @@ namespace MODL3_Sokoban.domain
 {
 	public class Movable
 	{
+        public Location currentLoc { get; set; }
+        public Symbol symbol { get; set; }
+        public bool canMoveObject { get; set; }
+
+        protected void Move()
+        {
+
+        }
+
+        protected void ChangeSymbol()
+        {
+
+        }
 	}
 }
