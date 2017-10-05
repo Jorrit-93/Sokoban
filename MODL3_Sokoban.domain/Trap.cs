3fb797aacc26f079b70404c2f46c60d37293aa4b
Unknown <mbeks1@avans.nl>
2017-10-05 11:38:44 +0200

    Trap-> stepcounter en DropCrate()

--- MODL3_Sokoban.domain/Trap.cs
+++ MODL3_Sokoban.domain/Trap.cs
@@ -2,8 +2,33 @@
 {
 	internal class Trap : BaseFloor
 	{
+        public int stepCounter { get; set; }
+
 		public Trap(int x, int y, char symbol) : base(x, y, symbol)
 		{
+            stepCounter = 0;
 		}
+
+        public void DropCrate()
+        {
+            if (_movable.currentLoc.xPosition == xPosition && _movable.currentLoc.yPosition == yPosition)
+            {
+                if (_movable.symbol == 'o')
+                {
+                    _movable = null;
+                }
+            }
+        }
+        
+        public void RaiseStepCounter()
+        {
+            stepCounter++;
+            if (stepCounter == 3)
+            {
+                symbol = ' ';
+                DropCrate();
+            }            
+        }
+
 	}
 }
